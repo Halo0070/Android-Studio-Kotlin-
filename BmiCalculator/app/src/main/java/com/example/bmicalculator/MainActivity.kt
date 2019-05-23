c55d2586df895b53eecca1249adc004173b1fb98
Halo0070 <lee998598@gmail.com>
2019-05-23 21:09:55 +0900

    Update

--- BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.bmicalculator
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        resultButton.setOnClickListener{
+            // 결과 버튼이 클릭되면 할 일을 작성하는 부분
+            val intent = Intent(this, ResultActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
