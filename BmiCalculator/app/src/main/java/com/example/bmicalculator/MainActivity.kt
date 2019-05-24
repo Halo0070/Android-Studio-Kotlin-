6b717ea3809452bc49afa0a1af6dcc9a14585eb8
Halo0070 <lee998598@gmail.com>
2019-05-24 23:53:02 +0900

    Update MainActivity.kt

--- BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.bmicalculator
 
 import android.os.Bundle
+import android.preference.PreferenceManager
 import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.startActivity
 
@@ -11,12 +13,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        resultButton.setOnClickListener{
+        resultButton.setOnClickListener {
+            //마지막에 입려한 내용 저장
+            saveData(heightEditText.text.toString().toInt(),
+                weightEditText.text.toString().toInt())
+            
             // 결과 버튼이 클릭되면 할 일을 작성하는 부분
             startActivity<ResultActivity>(
                 "weight" to weightEditText.text.toString(),
                 "height" to heightEditText.text.toString()
             )
         }
+        Toast.makeText(this, "bmi", Toast.LENGTH_SHORT).show()
+    }
+
+    //데이터 저장하기
+    private fun saveData(height: Int, weight: Int) {
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+
+        editor.putInt("KEY_HEIGHT", height)
+              .putInt("KEY_WEIGHT", weight)
+              .apply()
     }
 }
