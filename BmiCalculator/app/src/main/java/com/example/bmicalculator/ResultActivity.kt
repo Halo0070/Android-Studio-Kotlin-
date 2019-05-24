bd01808cb5be12298d58de5f376a707aeb567fd5
Halo0070 <lee998598@gmail.com>
2019-05-24 23:05:36 +0900

    Update ResultActivity.kt

--- BmiCalculator/app/src/main/java/com/example/bmicalculator/ResultActivity.kt
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/ResultActivity.kt
@@ -1,12 +1,19 @@
 package com.example.bmicalculator
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val intent = Intent(this, ResultActivity::class.java)
+        intent.putExtra("weight", weightEditText.text.toString())
+        intent.putExtra("height", heightEditText.text.toString())
+        startActivity(intent)
     }
 }
