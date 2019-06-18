7a4ea0415cbfd208212f7f44a7e1796637a2a699
Halo0070 <lee998598@gmail.com>
2019-06-18 23:21:26 +0900

    Update MainActivity.kt

--- StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ StopWatch/app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
+import kotlin.concurrent.timer
 
 class MainActivity : AppCompatActivity() {
     private var time = 0    // 시간을 계산할 변수를 0으로 초기화
@@ -29,5 +30,20 @@ class MainActivity : AppCompatActivity() {
     private fun pause() {
         fab.setImageResource(R.drawable.ic_play_arrow_black_24dp)   // 타이머 시작과 반대로 FAB를 클릭하면 이미지를 시작 이미지로 교체
         timerTask?.cancel()     // 실행 중인 타이머가 있다면 타이머를 취소
+    }
+
+    //타이머 시작 구현
+    private fun  start() {
+        fab.setImageResource(R.drawable.ic_pause_black_24dp)    // 타이머를 시작하는 FAB를 누르면 FAB의 이미지를 일시정지 이미지로 변경
+
+        timerTask = timer(period = 10) {    // Timer 객체를 변수에 저장
+            time++                          // 계산한 초와 밀리초를 각각의 텍스트 뷰에 설정
+            val sec = time / 100
+            val milli = time % 100
+            runOnUiThread{                  // runOnUiThread로 감싸서 UI 조작이 가능하게 함.
+                secTextView.text = "$sec"
+                milliTextView.text = "$milli"
+            }
+        }
     }
 }
