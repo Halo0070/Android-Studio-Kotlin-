3867fbbf4698735ca6074c68b594629856943753
Halo0070 <lee998598@gmail.com>
2019-07-12 21:36:38 +0900

    Update MainActivity.kt

--- MyWebBrowser/app/src/main/java/com/example/mywebbrowser/MainActivity.kt
+++ MyWebBrowser/app/src/main/java/com/example/mywebbrowser/MainActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.webkit.WebViewClient
 import kotlinx.android.synthetic.main.activity_main.*
@@ -25,14 +27,17 @@ class MainActivity : AppCompatActivity() {
         webView.loadUrl("http://www.google.com")    // loadUrl() 메서드를 사용하여 "http://'가 포함된 Url을 전달하면 웹뷰에서 페이지가 로딩됨.
 
         // 키보드의 검색 버튼 동작 정의하기
-        urlEditText.setOnEditorActionListener {_, actionId, _ ->    // 글자가 입력될 때마다 호출 인자로는 {반응한 뷰, 액션ID, 이벤트} 세가지, 뷰와 이벤트를 사용하지 않기 때문에 _로 대치 가능
-            if(actionId == EditorInfo.IME_ACTION_SEARCH) {      // actionId값은 EditorInfo 클래스에 상수로 정의된 값 중에서 검색 버튼에 해당하는 상수와 비교하여 검색 버튼이 눌렸는지 확인함.
+        urlEditText.setOnEditorActionListener { _, actionId, _ ->
+            // 글자가 입력될 때마다 호출 인자로는 {반응한 뷰, 액션ID, 이벤트} 세가지, 뷰와 이벤트를 사용하지 않기 때문에 _로 대치 가능
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {      // actionId값은 EditorInfo 클래스에 상수로 정의된 값 중에서 검색 버튼에 해당하는 상수와 비교하여 검색 버튼이 눌렸는지 확인함.
                 webView.loadUrl(urlEditText.text.toString())    // 검색 창에 입력한 주소를 웹뷰에 전달하여 로딩함. 마지막으로 true를 반환하여 이벤트를 종료함.
                 true
             } else {
                 false
             }
         }
+        // 컨텍스트 메뉴 등록
+        registerForContextMenu(webView)
     }
 
     // 뒤로가기 동작 재정의
@@ -81,5 +86,24 @@ class MainActivity : AppCompatActivity() {
             }
         }
         return super.onOptionsItemSelected(item)    // when문에서는 각 메뉴 처리를 끝내고 true를 반환했고 내가 처리하고자 하는 경우를 제외한 그 이외의 경우에는 super 메서드를 호출하는 것이 안드로이드 시스템에서의 보편적인 규칙임.
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.context, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem?): Boolean {
+        when(item?.itemId){
+            R.id.action_share -> {
+                // 페이지 공유
+                return true
+            }
+            R.id.action_browser -> {
+                // 기본 웹 브라우저에서 열기
+                return true
+            }
+        }
+        return super.onContextItemSelected(item)
     }
 }
