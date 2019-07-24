d7bdc93de3e7ed7b6a9edb2c49a072b65d8ae6ca
Halo0070 <lee998598@gmail.com>
2019-07-24 22:17:37 +0900

    Update MainActivity.kt

--- TiltSensor/app/src/main/java/com/example/tiltsensor/MainActivity.kt
+++ TiltSensor/app/src/main/java/com/example/tiltsensor/MainActivity.kt
@@ -26,10 +26,17 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         setContentView(R.layout.activity_main)
     }
 
+    // 센서 등록
     override fun onResume() {
         super.onResume()
         sensorManager.registerListener(this,        // registerListener() 메서드로 사용할 센서를 등록, this를 지정하여 액티비티에서 센서값을 받도록 함.
             sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),  // getDefaultSensor() 메서드로 사용할 센서 종류를 지정함. Sensor 클래스에 상수로 정의된 가속도(TYPE_ACCELEROMETER)를 지정함.
             SensorManager.SENSOR_DELAY_NORMAL)      // 세 번째 인자는 센서값을 얼마나 자주 받을지를 지정함. SensorManager 클래스에 정의된 상수 중 하나를 선택함.
+    }
+
+    // 센서 해제
+    override fun onPause() {
+        super.onPause()
+        sensorManager.unregisterListener(this)
     }
 }
