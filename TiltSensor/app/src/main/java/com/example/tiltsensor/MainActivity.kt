004bddd876808d379ada6e91d1a07a0f01405e90
Halo0070 <lee998598@gmail.com>
2019-07-25 19:41:54 +0900

    Update MainActivity.kt

--- TiltSensor/app/src/main/java/com/example/tiltsensor/MainActivity.kt
+++ TiltSensor/app/src/main/java/com/example/tiltsensor/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.tiltsensor
 
 import android.content.Context
+import android.content.pm.ActivityInfo
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
     override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
@@ -14,6 +17,14 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     override fun onSensorChanged(event: SensorEvent?) {
+        // 센서값이 변경되면 호출됨
+        // values[0] : x축 값 : 위로 기울이면 -10~0, 아래로 기울이면 0~10
+        // values[1] : y축 값 : 왼쪽으로 기울이면 -10~0, 오른쪽으로 기울이면 0~10
+        // values[2] : z축 값 : 미사용
+        event?.let {
+            Log.d("MainActivity", "onSensorChanged: x :" +
+            "${event.values[0]}, y : ${event.values[1]}, z : ${event.values[2]}")
+        }
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
@@ -22,6 +33,10 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        // 화면이 꺼지지 않게 하기
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        // 화면을 가로 모드로 고정
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
